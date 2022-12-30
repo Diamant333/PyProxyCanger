930fc860db2296bbb90bfdda7560d72d61870544
Alexey Mashchinov <resurs31@gmail.com>
2022-12-30 11:25:18 +0300

    Server created

--- main.py
+++ main.py
@@ -1,21 +1,23 @@
 from http.server import HTTPServer, BaseHTTPRequestHandler
 from urllib.parse import parse_qs
 
-dat = []
-
 
 class SimpleHTTPRequestHandler(BaseHTTPRequestHandler):
 
     def do_GET(self):
-        dat.append('res')
+        x = self.wfile.write
         qs = {}
         path = self.path
         if '?' in path:
             path, tmp = path.split('?', 1)
             qs = parse_qs(tmp)
+
         line_auth = 'http://{}/main?TECH_SENS_COUNT'.format(qs['ip_addr'][0])
+        self.send_response(200)
+        self.send_header("Content-type", "text/html")
+        self.end_headers()
+        x(b'Ok\n')
         print(line_auth)
-        return 'Ok'
 
 
 httpd = HTTPServer(('localhost', 8000), SimpleHTTPRequestHandler)
